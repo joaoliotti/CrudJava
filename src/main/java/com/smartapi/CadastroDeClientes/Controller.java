ab2a52427678c4bb8b9fedf81140a1dee5336318
joaoliotti <joaoliotti14@gmail.com>
2025-04-13 17:10:41 -0300

    first commit

---
+++ src/main/java/com/smartapi/CadastroDeClientes/Controller.java
@@ -0,0 +1,16 @@
+package com.smartapi.CadastroDeClientes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasvindas(){
+        return "Hello World";
+    }
+}
