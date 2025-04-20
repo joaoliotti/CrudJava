585af9e797ab52296585b43f6a799d2c01ddfafd
joaoliotti <joaoliotti14@gmail.com>
2025-04-20 03:03:52 -0300

    Adicionado Lombok ao projeto, para melhorar criação de construtores, getters e setters

--- src/main/java/com/smartapi/CadastroDeClientes/Missoes/MissoesModel.java
+++ src/main/java/com/smartapi/CadastroDeClientes/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package com.smartapi.CadastroDeClientes.Missoes;
 
 import com.smartapi.CadastroDeClientes.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
--- src/main/java/com/smartapi/CadastroDeClientes/Ninjas/NinjaModel.java
+++ src/main/java/com/smartapi/CadastroDeClientes/Ninjas/NinjaModel.java
@@ -2,11 +2,13 @@ package com.smartapi.CadastroDeClientes.Ninjas;
 
 import com.smartapi.CadastroDeClientes.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -21,37 +23,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // foreing key or chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
